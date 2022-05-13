beabbb993973907d9e6474889470d4707333be13
sandflavoured <saminhaque.sh@gmail.com>
2022-05-13 10:37:38 +1000

    Added functions for calibration and signal reading

--- prototype.ino
+++ prototype.ino
@@ -1,14 +1,29 @@
 #include <Mouse.h>
 
 
-int V_horizontal = 0;
-int V_vertical = 0;
+screenRes[2] = {2240, 1400};
+
+
+
+enum blink_t {
+    BLINK = 0,
+    BLINKBLINK,
+    LONGBLINK
+}
 
 
 void setup() {
 
     Serial.begin(9600);
     Mouse.begin();
+
+    // run calibration
+
+    
+
+
+    calibration();
+
 
 
     
@@ -22,10 +37,60 @@ void loop() {
     V_vertical = analogRead(A1);
 
     // Find current mouse position
-    
+        
 
     // Move mouse
     Mouse.move();
 
 
+}
+
+
+
+void calibration() {
+    
+
+    /* 
+     *  Running cal.....START
+     *  Look at the red dot, then blink twice.
+     */
+
+    Serial.print("Running calibration");
+
+    int i;
+    for (i = 0; i < 5; i++) {
+        Serial.print('.');
+        delay(200);
+    }
+
+    Serial.println("START");
+    Serial.println("Look at the red dot, then blink twice.");
+
+
+    // Calibrate using EOG data
+
+    int cal[3][2] = {{0,0},{0,0},{0,0}};
+
+    bool blinkblink = false;
+    blinkblink = (checkblink() == BLINKBLINK) ? true : false;
+
+    while (!blinkblink) {
+        blinkblink = (checkblink() == BLINKBLINK) ? true : false;
+    }
+
+    readSignal();
+
+}
+
+int checkblink() {
+
+}
+
+int *readSignal() {
+    vals[2] = {0,0};
+
+    vals[0] = analogRead(A0);
+    vals[1] = analogRead(A1);
+    
+    return vals;
 }
