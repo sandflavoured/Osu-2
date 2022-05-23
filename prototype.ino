480402c1361d63595ef0c44e0ce34e77bf1a249b
sandflavoured <saminhaque.sh@gmail.com>
2022-05-23 11:33:15 +1000

    No longer processing signal on Arduino
    The code in the Arduino will now simply read the signal voltage and control offsets.

--- prototype.ino
+++ prototype.ino
@@ -1,29 +1,18 @@
-#include <Mouse.h>
-
-
-//! set screen resolution here
-int screenRes[2] = {2240, 1400};
-
 
 // Global definitions
-
-typedef enum {
-    BLINK = 0,
-    BLINKBLINK,
-    LONGBLINK
-} blink_t;
+int *sig_vol;
 
 // ================== Setup ==================
 
 void setup() {
 
     Serial.begin(9600);
-    Mouse.begin();
+
 
     // run calibration
     //calibration();
 
-    Mouse.move(90,90,0);
+
 
     
 }
@@ -32,21 +21,9 @@ void setup() {
 
 void loop() {
 
-    // // Read from sensor
-    // V_horizontal = analogRead(A0);
-    // V_vertical = analogRead(A1);
+    sig_vol = readSignal();
 
-    // // Center mouse (every so often)
-    // //centerMouse();
-
-    // // Move mouse
-    // xMove = 0;
-    // yMove = 0;
-    // Mouse.move(xMove, yMove);
-
-    // if (clicked()) {
-    //     Mouse.click();
-    // }
+    Serial.println();
 
 
 }
@@ -55,45 +32,8 @@ void loop() {
 
 // ================== Functions ==================
 
-void calibration() {
-    
-
-    /* 
-     *  Running cal.....START
-     *  Look at the red dot, then blink twice.
-     */
-
-    Serial.print("Running calibration");
-
-    int i;
-    for (i = 0; i < 5; i++) {
-        Serial.print('.');
-        delay(200);
-    }
-
-    Serial.println("START");
-    delay(1000);
-    Serial.println("Look at the red dot, then blink twice.");
 
 
-    // Calibrate using EOG data
-
-    int cal[3][2] = {{0,0},{0,0},{0,0}};
-
-    bool blinkblink = false;
-    blinkblink = (checkblink() == BLINKBLINK) ? true : false;
-
-    while (!blinkblink) {
-        blinkblink = (checkblink() == BLINKBLINK) ? true : false;
-    }
-
-    readSignal();
-
-}
-
-int checkblink() {
-
-}
 
 int *readSignal() {
     vals[2] = {0,0};
@@ -104,3 +44,13 @@ int *readSignal() {
     return vals;
 }
 
+void offset() {
+    int vals[2] = readSignal();
+
+    int offSetX = map();
+    int offSetY = map();
+
+    analogWrite(10, offSetX);
+    analogWrite(10, offSetY);
+
+}
