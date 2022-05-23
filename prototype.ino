979a567ea21eb0270323396cbb271c64025e058f
sandflavoured <saminhaque.sh@gmail.com>
2022-05-23 12:37:21 +1000

    Working on offset function
    [Problem] map() uses integer math so not accurate, need to make own map function for float and verify.

--- prototype.ino
+++ prototype.ino
@@ -32,11 +32,15 @@ void loop() {
 
 // ================== Functions ==================
 
+int map_a(float x, float in_min, float in_max, float out_min, float out_max) {
 
+    return (int)((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+
+}
 
 
 int *readSignal() {
-    vals[2] = {0,0};
+    int *vals;
 
     vals[0] = analogRead(A0);
     vals[1] = analogRead(A1);
@@ -45,12 +49,48 @@ int *readSignal() {
 }
 
 void offset() {
-    int vals[2] = readSignal();
+    int *vals = readSignal();     // 0 to 1024
 
-    int offSetX = map();
-    int offSetY = map();
+    float voltage[2] = {
+        (float)vals[0] / 1024.0 * 5.0, 
+        (float)vals[1] / 1024.0 * 5.0
+    };       // 0 to 5
 
-    analogWrite(10, offSetX);
-    analogWrite(10, offSetY);
+
+    // initialise offsets
+    float offSetX_volt = 0;
+    float offSetY_volt = 0;
+
+
+    // Set X offset
+    if (voltage[0] >= 5) {
+        offSetX_volt = -4.0;
+    }
+    else if (voltage[0] <= 0) {
+        offSetX_volt = 4.0;
+    }
+    else {
+        offSetX_volt = 2.5 - voltage[0];
+    }
+
+
+    // Set Y offset
+    if (voltage[1] >= 5) {
+        offSetY_volt = -4.0;
+    }
+    else if (voltage[1] <= 0) {
+        offSetY_volt = 4.0;
+    }
+    else {
+        offSetY_volt = 2.5 - voltage[1];
+    }
+
+
+    int offSetX = map(offSetX_volt, 0, 5, 0, 255);
+
+    analogWrite(10, offSetX_volt);
+    analogWrite(10, offSetY_volt);
+
+
 
 }
